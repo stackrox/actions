8d1b982f2fd2c7c7bb6e5929b02e65aa20287baa
Tom Martensen <tmartens@redhat.com>
2023-08-09 08:57:44 +0200

    sanity

--- release/wait-for-image/wait-for-image.sh
+++ release/wait-for-image/wait-for-image.sh
@@ -19,19 +19,15 @@ check_not_empty \
     INTERVAL \
     TIME_LIMIT
 
-echo "hello world"
-
 find_tag() {
     URL="https://quay.io/api/v1/repository/$1/tag?specificTag=$2"
-    curl --location "$URL" \
-                -H "Authorization: Bearer $TOKEN"
     {
         if [ -z "$TOKEN" ]; then
             gh_log notice "Connecting to Quay without token"
             curl --silent --show-error --fail --location "$URL"
         else
             gh_log notice "Connecting to Quay with token"
-            curl --show-error --fail --location "$URL" \
+            curl --silent --show-error --fail --location "$URL" \
                 -H "Authorization: Bearer $TOKEN"
         fi
     } | jq -r ".tags[0].name"
