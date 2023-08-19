a471d991138b1eec649f744d4654ac5182b7528a
JoukoVirtanen <jouko@stackrox.com>
2023-08-19 10:12:40 -0700

    Removed debugging

--- release/start-acs/start-acs.sh
+++ release/start-acs/start-acs.sh
@@ -51,8 +51,5 @@ ROX_ADMIN_PASSWORD=$(cat "${STACKROX_DIR}"/deploy/k8s/central-deploy/password)
 kubectl -n stackrox create secret generic access-rhacs --from-literal="username=${ROX_ADMIN_USERNAME}" --from-literal="password=${ROX_ADMIN_PASSWORD}" --from-literal="central_url=https://${CENTRAL_IP}"
 echo "rox_password=${ROX_ADMIN_PASSWORD}" >> "$GITHUB_OUTPUT"
 echo "central-ip=${CENTRAL_IP}" >> "$GITHUB_OUTPUT"
-ls $GITHUB_OUTPUT
-echo ""
-cat $GITHUB_OUTPUT
 
 printf "Long-running GKE cluster %s has been patched.\nAccess it by running \`./scripts/release-tools/setup-central-access.sh %s\` from your local machine." "${NAME//./-}" "${NAME//./-}" >> "$GITHUB_STEP_SUMMARY"
