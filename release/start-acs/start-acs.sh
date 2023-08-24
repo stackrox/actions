b89a1763c2688a05ea239c429fa3315bab373a8f
Michaël Petrov <smartptr@gmail.com>
2023-08-24 10:28:56 +0200

    more fixes

--- release/start-acs/start-acs.sh
+++ release/start-acs/start-acs.sh
@@ -24,12 +24,27 @@
 
 set -euo pipefail
 
-pushd "$STACKROX_DIR"
-
-# shellcheck source=/dev/null
-source "${STACKROX_DIR}"/deploy/common/deploy.sh
-
 "${STACKROX_DIR}"/deploy/k8s/central.sh
+
+PATCH=$(cat <<EOPATCH
+{ "spec": { "template":
+    { "spec": { "containers": [
+        { "name": "central",
+            "env": [
+                { "name": "MUTEX_WATCHDOG_TIMEOUT_SECS", "value": "0" },
+                { "name": "ROX_TELEMETRY_STORAGE_KEY_V1", "value": "R5fMyO9n0gibSGzOXtlP2qCFWCGb8uoW" }
+            ],
+            "resources": {
+                "requests": { "memory": "3Gi", "cpu": "2" },
+                "limits": { "memory": "12Gi", "cpu": "4" }
+            }
+        }
+    ] } }
+} }
+EOPATCH
+)
+kubectl -n stackrox patch deploy/central -p "$PATCH"
+
 kubectl -n stackrox port-forward deploy/central 8000:8443 > /dev/null 2>&1 &
 sleep 20
 
@@ -55,40 +70,22 @@ EOPATCH
 )
 kubectl -n stackrox patch deploy/sensor -p "$PATCH"
 
-PATCH=$(cat <<EOPATCH
-{ "spec": { "template":
-    { "spec": { "containers": [
-        { "name": "central",
-            "env": [
-                { "name": "MUTEX_WATCHDOG_TIMEOUT_SECS", "value": "0" },
-                { "name": "ROX_TELEMETRY_STORAGE_KEY_V1", "value": "R5fMyO9n0gibSGzOXtlP2qCFWCGb8uoW" }
-            ],
-            "resources": {
-                "requests": { "memory": "3Gi", "cpu": "2" },
-                "limits": { "memory": "12Gi", "cpu": "4" }
-            }
-        }
-    ] } }
-} }
-EOPATCH
-)
-kubectl -n stackrox patch deploy/central -p "$PATCH"
-
 CENTRAL_IP=$(kubectl -n stackrox get svc/central-loadbalancer -o json | jq -r '.status.loadBalancer.ingress[0] | .ip // .hostname')
 
 API_ENDPOINT="${CENTRAL_IP}:443"
 wait_for_central "${API_ENDPOINT}"
 
 ROX_ADMIN_PASSWORD=$(cat "${STACKROX_DIR}"/deploy/k8s/central-deploy/password)
+
+gh_output rox-password "$ROX_ADMIN_PASSWORD"
+gh_output central-ip "$CENTRAL_IP"
+
 echo "::add-mask::$ROX_ADMIN_PASSWORD"
 
 kubectl -n stackrox create secret generic access-rhacs \
     --from-literal="username=${ROX_ADMIN_USERNAME}" \
     --from-literal="password=${ROX_ADMIN_PASSWORD}" \
     --from-literal="central_url=https://${CENTRAL_IP}"
-
-gh_output rox-password "$ROX_ADMIN_PASSWORD"
-gh_output central-ip "$CENTRAL_IP"
 
 gh_summary <<EOSUMMARY
 Long-running GKE cluster ${NAME//./-} has been patched.
