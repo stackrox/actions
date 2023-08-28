a19d8183d8e262e1d9cd50ba69147f169b657596
Michaël Petrov <smartptr@gmail.com>
2023-08-28 12:23:16 +0200

    kube burner action path

--- release/start-kube-burner/start-kube-burner.sh
+++ release/start-kube-burner/start-kube-burner.sh
@@ -9,16 +9,21 @@
 
 set -euo pipefail
 
+gh_log notice "Deploying benchmark operator"
 make -C "$BENCHMARK_OPERATOR_DIR" deploy
 
 node_name="$(kubectl get nodes -o=jsonpath='{.items[0].metadata.name}')"
 kube_burner_cr="$KUBE_BURNER_CONFIG_DIR"/kube-burner-cr.yml
 kube_burner_cr_gen="$KUBE_BURNER_CONFIG_DIR"/kube-burner-cr-gen.yml
+
+gh_log notice "Patching $kube_burner_cr"
 sed "s|__NODE_NAME__|$node_name|" "$kube_burner_cr" > "$kube_burner_cr_gen"
 
 dockerconfigjson="$(kubectl -n stackrox get secret stackrox -o yaml | grep dockerconfigjson | head -1 | awk '{print $2}')"
 secret_template="$KUBE_BURNER_CONFIG_DIR"/secret_template.yml
 secret_file="$KUBE_BURNER_CONFIG_DIR"/cluster-density/secret.yml
+
+gh_log notice "Patching $secret_template"
 sed "s|__DOCKERCONFIGJSON__|$dockerconfigjson|" "$secret_template" > "$secret_file" 
 
 kubectl create configmap --from-file="$KUBE_BURNER_CONFIG_DIR/cluster-density" kube-burner-config -n benchmark-operator
