da9b2b9864b8f0e4cb461f06a8646c5934280e22
Michaël <smartptr@gmail.com>
2023-09-06 16:51:10 +0200

    ROX-19190: refactoring (#23)

--- release/start-acs/start-acs.sh
+++ release/start-acs/start-acs.sh
@@ -24,33 +24,50 @@
 
 set -euo pipefail
 
-pushd "$STACKROX_DIR"
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 
 # shellcheck source=/dev/null
 source "${STACKROX_DIR}"/deploy/common/deploy.sh
 
+gh_log notice "Deploying central..."
 "${STACKROX_DIR}"/deploy/k8s/central.sh
+
+gh_log notice "Forwarding central port..."
 kubectl -n stackrox port-forward deploy/central 8000:8443 > /dev/null 2>&1 &
 sleep 20
 
+gh_log notice "Deploying sensor..."
 "${STACKROX_DIR}"/deploy/k8s/sensor.sh
 
-kubectl -n stackrox set env deploy/sensor MUTEX_WATCHDOG_TIMEOUT_SECS=0 ROX_FAKE_KUBERNETES_WORKLOAD=long-running ROX_FAKE_WORKLOAD_STORAGE=/var/cache/stackrox/pebble.db
-kubectl -n stackrox patch deploy/sensor -p '{"spec":{"template":{"spec":{"containers":[{"name":"sensor","resources":{"requests":{"memory":"3Gi","cpu":"2"},"limits":{"memory":"12Gi","cpu":"4"}}}]}}}}'
+gh_log notice "Patching sensor deployment..."
+kubectl -n stackrox patch deploy/sensor --patch-file="${SCRIPT_DIR}/patch-sensor.json"
 
-kubectl -n stackrox set env deploy/central MUTEX_WATCHDOG_TIMEOUT_SECS=0
-kubectl -n stackrox patch deploy/central -p '{"spec":{"template":{"spec":{"containers":[{"name":"central","resources":{"requests":{"memory":"3Gi","cpu":"2"},"limits":{"memory":"12Gi","cpu":"4"}}}]}}}}'
+gh_log notice "Patching central deployment..."
+kubectl -n stackrox patch deploy/central --patch-file="${SCRIPT_DIR}/patch-central.json"
 
 CENTRAL_IP=$(kubectl -n stackrox get svc/central-loadbalancer -o json | jq -r '.status.loadBalancer.ingress[0] | .ip // .hostname')
+gh_log notice "CENTRAL_IP=$CENTRAL_IP"
 
-API_ENDPOINT="${CENTRAL_IP}":443
+API_ENDPOINT="${CENTRAL_IP}:443"
 wait_for_central "${API_ENDPOINT}"
 
 ROX_ADMIN_PASSWORD=$(cat "${STACKROX_DIR}"/deploy/k8s/central-deploy/password)
-# TODO ROX-19190 Mask $ROX_ADMIN_PASSWORD
-#echo "::add-mask::$ROX_ADMIN_PASSWORD"
-kubectl -n stackrox create secret generic access-rhacs --from-literal="username=${ROX_ADMIN_USERNAME}" --from-literal="password=${ROX_ADMIN_PASSWORD}" --from-literal="central_url=https://${CENTRAL_IP}"
-echo "rox_password=${ROX_ADMIN_PASSWORD}" >> "$GITHUB_OUTPUT"
-echo "central-ip=${CENTRAL_IP}" >> "$GITHUB_OUTPUT"
 
-printf "Long-running GKE cluster %s has been patched.\nAccess it by running \`./scripts/release-tools/setup-central-access.sh %s\` from your local machine." "${NAME//./-}" "${NAME//./-}" >> "$GITHUB_STEP_SUMMARY"
+# Don't mask the password: masked values are not passed to the runner.
+gh_output rox-password "$ROX_ADMIN_PASSWORD"
+gh_output central-ip "$CENTRAL_IP"
+
+gh_log notice "Creating access-rhacs secret with the username and the password..."
+kubectl -n stackrox create secret generic access-rhacs \
+    --from-literal="username=${ROX_ADMIN_USERNAME}" \
+    --from-literal="password=${ROX_ADMIN_PASSWORD}" \
+    --from-literal="central_url=https://${CENTRAL_IP}"
+
+gh_summary <<EOSUMMARY
+Long-running GKE cluster ${NAME//./-} has been patched.
+Access it by running:
+\`\`\`
+./scripts/release-tools/setup-central-access.sh ${NAME//./-}
+\`\`\`
+from your local machine.
+EOSUMMARY
