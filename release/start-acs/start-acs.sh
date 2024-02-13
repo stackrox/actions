efe970d28eaf3c959b7d09e299585601f681943d
JoukoVirtanen <jouko@stackrox.com>
2024-02-12 21:40:27 -0800

    Giving correct permissions to roxctl

--- release/start-acs/start-acs.sh
+++ release/start-acs/start-acs.sh
@@ -30,6 +30,7 @@ SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 source "${STACKROX_DIR}"/deploy/common/deploy.sh
 
 alias roxctl="${STACKROX_DIR}"/bin/linux_arm64/roxctl
+chmod 755 "${STACKROX_DIR}"/bin/linux_arm64/roxctl
 
 gh_log notice "Deploying central..."
 "${STACKROX_DIR}"/deploy/k8s/central.sh
