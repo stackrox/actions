fac0a4f101e2ce8d167151e321ca86f6cbe18f23
Jouko Virtanen <jvirtane@redhat.com>
2025-04-11 08:39:46 -0700

    Apply suggestions from code review
    Co-authored-by: Tom Martensen <tmartens@redhat.com>

--- release/start-kube-burner/start-kube-burner.sh
+++ release/start-kube-burner/start-kube-burner.sh
@@ -12,7 +12,7 @@ set -euo pipefail
 gh_log notice "Deploying benchmark operator"
 make -C "$BENCHMARK_OPERATOR_DIR" deploy
 
-# TODO: When all versions using the cluster-density directory
+# TODO(ROX-...): When all versions using the cluster-density directory
 # are out of support, remove it here
 kube_burner_load_dir="$KUBE_BURNER_CONFIG_DIR"/berserker-load
 if [ ! -d "$kube_burner_load_dir" ]; then
@@ -28,7 +28,7 @@ sed "s|__NODE_NAME__|$node_name|" "$kube_burner_cr" > "$kube_burner_cr_gen"
 
 dockerconfigjson="$(kubectl -n stackrox get secret stackrox -o yaml | grep dockerconfigjson | head -1 | awk '{print $2}')"
 secret_template="$KUBE_BURNER_CONFIG_DIR"/secret_template.yml
-secret_file="$kube_burner_load_dir"/secret.yml
+secret_file="${kube_burner_load_dir}/secret.yml"
 
 gh_log notice "Patching $secret_template"
 sed "s|__DOCKERCONFIGJSON__|$dockerconfigjson|" "$secret_template" > "$secret_file" 
