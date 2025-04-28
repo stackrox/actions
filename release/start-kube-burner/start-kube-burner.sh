fb7aaf83b089b1810854d37229442a1487656af3
JoukoVirtanen <jouko@stackrox.com>
2025-04-28 12:02:08 -0700

    Added kube-burner for central. Made the kube-burner config dir configurable

--- release/start-kube-burner/start-kube-burner.sh
+++ release/start-kube-burner/start-kube-burner.sh
@@ -4,20 +4,22 @@ DIR="$(cd "$(dirname "$0")" && pwd)"
 
 # TODO(ROX-28948): When all versions using the cluster-density directory
 # are out of support, remove it here
-kube_burner_load_dir="${KUBE_BURNER_CONFIG_DIR}/berserker-load"
-if [ ! -d "$kube_burner_load_dir" ]; then
-  kube_burner_load_dir="${KUBE_BURNER_CONFIG_DIR}/cluster-density"
-fi
+#kube_burner_load_dir="${KUBE_BURNER_CONFIG_DIR}/berserker-load"
+#if [ ! -d "$kube_burner_load_dir" ]; then
+#  kube_burner_load_dir="${KUBE_BURNER_CONFIG_DIR}/cluster-density"
+#fi
+
+KUBE_BURNER_CONFIG_DIR_BASE="$(dirname "$KUBE_BURNER_CONFIG_DIR")"
 
 dockerconfigjson="$(kubectl -n stackrox get secret stackrox -o yaml | grep dockerconfigjson | head -1 | awk '{print $2}')"
-secret_template="${KUBE_BURNER_CONFIG_DIR}/secret_template.yml"
-secret_file="${kube_burner_load_dir}/secret.yml"
+secret_template="${KUBE_BURNER_CONFIG_DIR_BASE}/secret_template.yml"
+secret_file="${KUBE_BURNER_CONFIG_DIR}/secret.yml"
 
 #gh_log notice "Patching $secret_template"
 sed "s|__DOCKERCONFIGJSON__|$dockerconfigjson|" "$secret_template" > "$secret_file" 
 
-kube_burner_config_map="${KUBE_BURNER_CONFIG_DIR}/kube-burner-config.yml"
-"${DIR}"/combine-configs.sh "$kube_burner_load_dir" kube-burner-config kube-burner > "$kube_burner_config_map"
+kube_burner_config_map="${KUBE_BURNER_CONFIG_DIR_BASE}/kube-burner-config.yml"
+"${DIR}"/combine-configs.sh "$KUBE_BURNER_CONFIG_DIR" kube-burner-config kube-burner > "$kube_burner_config_map"
 
 kubectl create ns kube-burner
 
