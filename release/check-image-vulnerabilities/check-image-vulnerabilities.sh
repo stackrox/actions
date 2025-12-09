7025a2a3b678d3e74869f723273a9a5615289203
Tom Martensen <tmartens@redhat.com>
2025-12-09 13:12:50 +0100

    handle no vulnerabilities found

--- release/check-image-vulnerabilities/check-image-vulnerabilities.sh
+++ release/check-image-vulnerabilities/check-image-vulnerabilities.sh
@@ -17,18 +17,45 @@ check_not_empty \
     VERSION \
     SUMMARY_PREFIX
 
-function count_fixable_vulnerabilities() {
-  local severity="$1"
-  local result_path="$2"
-  jq "[.result.vulnerabilities[] | select(.cveSeverity == \"$severity\" and .componentFixedVersion != \"\")] | length" "$result_path"
+function main() {
+  local image="$1"
+  local version="$2"
+  local summary_prefix="$3"
+  local result_path="scan-result.json"
+
+  # Scan the image for vulnerabilities.
+  scan_image "$image" "$version" "$result_path"
+
+  # Count the number of vulnerabilities and fixable vulnerabilities for each severity.
+  # Use associative arrays to store counts by severity.
+  # Arrays are used via nameref parameters in print_vulnerability_status function.
+  # shellcheck disable=SC2034
+  declare -A vuln_counts
+  # shellcheck disable=SC2034
+  declare -A fixable_counts
+
+  # shellcheck disable=SC2034
+  for severity in CRITICAL IMPORTANT MODERATE; do
+    vuln_counts[$severity]=$(count_vulnerabilities "$severity" "$result_path")
+    fixable_counts[$severity]=$(count_fixable_vulnerabilities "$severity" "$result_path")
+  done
+
+  # Print the summary of the vulnerabilities.
+  gh_summary "### $summary_prefix $image:$version"
+  print_vulnerability_status vuln_counts fixable_counts
+
+  # Print the vulnerabilities table in a collapsible section.
+  # For the table to render correctly, we need to add a newline after the summary.
+  gh_summary "<details><summary>Click to expand details</summary>\n"
+  gh_summary "$(print_table "$result_path")"
+  gh_summary "</details>"
+
+  if [[ -f failure_flag ]]; then
+    exit 1
+  fi
 }
 
-function count_vulnerabilities() {
-  local severity="$1"
-  local result_path="$2"
-  jq ".result.summary.$severity" "$result_path"
-}
-
+# Scans the image for vulnerabilities.
 function scan_image() {
   local image="$1"
   local version="$2"
@@ -39,21 +66,21 @@ function scan_image() {
   gh_output result-path "$result_path"
 }
 
-# Prints a markdown table of the vulnerabilities, sorted by severity.
-function print_table() {
-  local result_path="$1"
-    jq -r '
-        .result.vulnerabilities // []
-        | (["COMPONENT","VERSION","CVE","SEVERITY","FIXED_VERSION","LINK"] | @csv),
-        (["---","---","---","---","---","---"] | @csv),
-        (.[] | [.componentName // "", .componentVersion // "", .cveId // "", .cveSeverity // "", .componentFixedVersion // "", .cveInfo // ""] | @csv)
-    ' <(cat "$result_path") \
-    | sed 's/,/ | /g' \
-    | sed 's/^/| /' \
-    | sed 's/$/ |/' \
-    | sed 's/"//g'
+# Counts the number of vulnerabilities for a given severity.
+function count_vulnerabilities() {
+  local severity="$1"
+  local result_path="$2"
+  jq ".result.summary.$severity" "$result_path"
 }
 
+# Counts the number of fixable vulnerabilities for a given severity.
+function count_fixable_vulnerabilities() {
+  local severity="$1"
+  local result_path="$2"
+  jq "[.result.vulnerabilities // [] | .[] | select(.cveSeverity == \"$severity\" and .componentFixedVersion != \"\")] | length" "$result_path"
+}
+
+# Prints the vulnerability status and an overview table of the vulnerabilities counts.
 function print_vulnerability_status() {
   local -n vuln_counts_ref=$1
   local -n fixable_counts_ref=$2
@@ -74,33 +101,19 @@ function print_vulnerability_status() {
   done
 }
 
-result_path="scan-result.json"
-scan_image "$IMAGE" "$VERSION" "$result_path"
+# Prints a markdown table of the vulnerabilities, sorted by severity.
+function print_table() {
+  local result_path="$1"
+    jq -r '
+        .result.vulnerabilities // []
+        | (["COMPONENT","VERSION","CVE","SEVERITY","FIXED_VERSION","LINK"] | @csv),
+        (["---","---","---","---","---","---"] | @csv),
+        (.[] | [.componentName // "", .componentVersion // "", .cveId // "", .cveSeverity // "", .componentFixedVersion // "", .cveInfo // ""] | @csv)
+    ' <(cat "$result_path") \
+    | sed 's/,/ | /g' \
+    | sed 's/^/| /' \
+    | sed 's/$/ |/' \
+    | sed 's/"//g'
+}
 
-# Count the number of vulnerabilities and fixable vulnerabilities for each severity.
-# Use associative arrays to store counts by severity.
-# Arrays are used via nameref parameters in print_vulnerability_status function.
-# shellcheck disable=SC2034
-declare -A vuln_counts
-# shellcheck disable=SC2034
-declare -A fixable_counts
-
-# shellcheck disable=SC2034
-for severity in CRITICAL IMPORTANT MODERATE; do
-  vuln_counts[$severity]=$(count_vulnerabilities "$severity" "$result_path")
-  fixable_counts[$severity]=$(count_fixable_vulnerabilities "$severity" "$result_path")
-done
-
-# Print the summary of the vulnerabilities.
-gh_summary "### $SUMMARY_PREFIX $IMAGE:$VERSION"
-print_vulnerability_status vuln_counts fixable_counts
-
-# Print the vulnerabilities table in a collapsible section.
-# For the table to render correctly, we need to add a newline after the summary.
-gh_summary "<details><summary>Click to expand details</summary>\n"
-gh_summary "$(print_table "$result_path")"
-gh_summary "</details>"
-
-if [[ -f failure_flag ]]; then
-  exit 1
-fi
+main "$IMAGE" "$VERSION" "$SUMMARY_PREFIX"
