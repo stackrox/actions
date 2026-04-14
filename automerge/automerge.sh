831e3d0b1dcf147b94f16a3a092eec3798b77feb
Tom Martensen <tmartens@redhat.com>
2026-04-14 13:41:30 +0200

    use simplification for allowed_authors

--- automerge/automerge.sh
+++ automerge/automerge.sh
@@ -76,8 +76,7 @@ function main() {
         fi
 
         # Approve only PRs by allowed authors
-        IFS=',' read -r -a ALLOWED_AUTHORS_ARRAY <<< "${ALLOWED_AUTHORS}"
-        if [[ " ${ALLOWED_AUTHORS_ARRAY[*]} " == *"${AUTHOR}"* ]]; then
+        if [[ ",${ALLOWED_AUTHORS}," == *",${AUTHOR},"* ]]; then
             if [[ "${DRY_RUN}" == "true" ]]; then
                 echo "[DEBUG] ✓ PR #${PR_NUMBER} - would have approved [DRY RUN]"
             else
