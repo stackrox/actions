35a582c260c6fd63dee3b1dfb9984e7549a6dac5
Tomasz Janiszewski <janiszt@gmail.com>
2026-04-27 16:44:12 +0200

    Enhance error message for existing Central deployment
    Updated error message for existing Central deployment to provide more context.

--- release/start-acs/start-acs.sh
+++ release/start-acs/start-acs.sh
@@ -31,7 +31,7 @@ SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 source "${STACKROX_DIR}"/deploy/common/deploy.sh
 
 if kubectl -n stackrox get deploy/central; then
-  gh_log error "Central is already running"
+  gh_log error "Central is already running. It means that you are trying to create a long running cluster for a release for the second time when the first cluster already exists. Try looking for another workflow that created the cluster."
   exit 1
 fi
 
