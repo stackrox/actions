181d45d31bd936bcae94b4a29740794f7bee05c4
Marcin Owsiany <porridge@redhat.com>
2026-07-09 10:53:20 +0200

    Refactor infractl installation script
    Updated the script to download and decode infractl using a temporary file.

--- infra/install-infractl/install-infractl.sh
+++ infra/install-infractl/install-infractl.sh
@@ -9,10 +9,11 @@ if [[ ":$PATH:" != *":$HOME/.local/bin:"* ]]; then
   echo "$HOME/.local/bin" >> "$GITHUB_PATH"
 fi
 echo "Downloading infractl..."
+
+upgrade_data="$(mktemp)"
+trap 'rm -f "${upgrade_data}"' EXIT
 curl --fail --silent --show-error --retry 5 --retry-all-errors --retry-delay 5 --location \
-https://infra.rox.systems/v1/cli/linux/amd64/upgrade \
-| jq -r ".result.fileChunk" \
-| base64 -d \
-> ~/.local/bin/infractl
+  -o "${upgrade_data}" https://infra.rox.systems/v1/cli/linux/amd64/upgrade
+jq -r ".result.fileChunk | @base64d" "${upgrade_data}" > ~/.local/bin/infractl
 chmod +x ~/.local/bin/infractl
 infractl --version
