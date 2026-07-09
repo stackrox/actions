51590bfb28e5c3892322f87774cb28300115f161
Marcin Owsiany <porridge@redhat.com>
2026-07-09 12:13:21 +0200

    Increase curl retry attempts in install script
    Increased curl retry attempts from 5 to 20 for more robust downloads and keep default exponential backoff:
    
    https://everything.curl.dev/usingcurl/downloads/retry.html#tweak-your-retries

--- infra/install-infractl/install-infractl.sh
+++ infra/install-infractl/install-infractl.sh
@@ -12,7 +12,7 @@ echo "Downloading infractl..."
 
 upgrade_data="$(mktemp)"
 trap 'rm -f "${upgrade_data}"' EXIT
-curl --fail --silent --show-error --retry 5 --retry-all-errors --retry-delay 5 --location \
+curl --fail --silent --show-error --retry 20 --retry-all-errors --location \
   --output "${upgrade_data}" https://infra.rox.systems/v1/cli/linux/amd64/upgrade
 # jq's @base64d is not binary-safe
 jq -r .result.fileChunk "${upgrade_data}" | base64 -d > ~/.local/bin/infractl
