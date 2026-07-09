4bd4a1d6c6b5cfde8a3d2ad72c84532015aabf8e
Marcin Owsiany <porridge@redhat.com>
2026-07-09 10:47:23 +0200

    Update infra/install-infractl/install-infractl.sh
    Co-authored-by: Misha Sugakov <537715+msugakov@users.noreply.github.com>

--- infra/install-infractl/install-infractl.sh
+++ infra/install-infractl/install-infractl.sh
@@ -8,6 +8,7 @@ if [[ ":$PATH:" != *":$HOME/.local/bin:"* ]]; then
   PATH="$HOME/.local/bin:$PATH"
   echo "$HOME/.local/bin" >> "$GITHUB_PATH"
 fi
+echo "Downloading infractl..."
 curl --fail --silent --show-error --retry 5 --retry-all-errors --retry-delay 5 --location \
 https://infra.rox.systems/v1/cli/linux/amd64/upgrade \
 | jq -r ".result.fileChunk" \
